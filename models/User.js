44346491857db1f1c665b307b74ed713b0dc69d1
Abhishek Kuntare <abhishekkuntare7@gmail.com>
2023-12-09 17:22:46 +0530

    change admin

--- models/User.js
+++ models/User.js
@@ -24,7 +24,7 @@ const schema = new mongoose.Schema({
   role: {
     type: String,
     enum: ["admin", "user"],
-    default: "user",
+    default: "admin",
   },
 
   avatar: {
@@ -37,7 +37,7 @@ const schema = new mongoose.Schema({
       required: true,
     },
   },
- 
+
   createdAt: {
     type: Date,
     default: Date.now,
